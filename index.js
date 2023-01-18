60e75fcdc1e130e1b27499c4ea0ad38a2c004e5f
Brady Agranoff <bradyagranoff@bradys-mbp-2.lan>
2023-01-18 11:46:17 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = new Configuration({
+	apiKey: "YOUR_API_KEY",
+});
+
+const openai = new OpenAIApi(config);
+
+const runPrompt = async () => {
+	const prompt = `
+        write me a joke about a cat and a bowl of pasta. Return response in the following parsable JSON format:
+
+        {
+            "Q": "question",
+            "A": "answer"
+        }
+
+    `;
+
+	const response = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: prompt,
+		max_tokens: 2048,
+		temperature: 1,
+	});
+
+	const parsableJSONresponse = response.data.choices[0].text;
+	const parsedResponse = JSON.parse(parsableJSONresponse);
+
+	console.log("Question: ", parsedResponse.Q);
+	console.log("Answer: ", parsedResponse.A);
+};
+
+runPrompt();
